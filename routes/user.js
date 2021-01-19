1d47651f8437877b8c560d4baebd85b4a55f081b
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-01-19 12:52:03 +0530

    routes added

---
+++ routes/user.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { check, validationResult} = require("express-validator/check");
+const auth = require("../middleware/auth");
+const router = express.Router()
+
+const usercontroller = require('../controllers/usercontroller')
+const User = require('../models/usermodel')
+
+router.post('/register',[
+    check("username", "Please Enter a Valid Username")
+    .not()
+    .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+        min: 6
+    })
+], usercontroller.register)
+router.post('/login',[
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+      min: 6
+    })
+  ], usercontroller.login)
+
+// router.get("/login",function (req, res) { 
+//     res.render("login"); 
+// });
+
+// router.get("/register", function (req, res) { 
+//     res.render("register"); 
+// });
+
+router.get("/me", auth, async (req, res) => {
+    try {
+      // request.user is getting fetched from Middleware after token authentication
+      const user = await User.findById(req.user.id);
+      res.json(user);
+    } catch (e) {
+      res.send({ message: "Error in Fetching user" });
+    }
+  });
+
+module.exports = router
