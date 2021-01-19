634daa083418c5b3452d3e7bfc1aa4d9b8cc5bc4
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-01-19 12:53:23 +0530

    middleware and cors addded

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const user = require("./routes/user");
 const initiatemongoserver = require("./config/db");
 
 initiatemongoserver();
+
 const app = express();
 
 // PORT
 const PORT = process.env.PORT || 4000;
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
 
-
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
+
+app.use(cors());
+app.use("/", user);
