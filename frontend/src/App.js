268ec46c143758249c6f554b80872728b499028d
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-05 13:28:16 +0530

    user registration done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Insta from './Insta' ;
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
+// import Login from './components/Login';
+import Registration from './components/Registration';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Insta/>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render(){
+    return (
+
+      <Router>
+        <div className="App">
+          <Switch>
+              <Route exact path="/register" component={Registration} />
+              {/* <Route exact path="/login" component={Login} />
+              <Redirect from="/" to="login" /> */}
+          </Switch>
+        </div>
+      </Router>
+      
+    );
+  };
+};
 
 export default App;
---
+++ frontend/src/elements/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = ({ message }) => (
+	<div>
+		<span className="message">{message}</span>
+	</div>
+);
+
+export default Error;
