629990f418c923065fcd9a0ef70a5bbf37a4728c
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-06 15:35:19 +0530

    login component started frontend created

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,10 +2,65 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Registration extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isLoading : true,
+            token:'',
+            LoginUsername:'',
+            LoginEmail:'',
+            LoginPassword:'',
+            login: false,
+            error: false,
+            LoginError: '',
+            fieldError:{}
+        };
+    }
+
     render(){
+
+        const {
+            isLoading,
+            token,
+            LoginUsername,
+            LoginEmail,
+            LoginPassword,
+            login,
+            error,
+            LoginError,
+            fieldError
+            } = this.state;
+
+
+
         return(
             <div>
                 <h1>LOGIN</h1>
+                {' '}
+                <div>
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    autoComplete="Email"
+                    value={LoginEmail}
+                    onChange={this.onTextboxChangeLoginEmail}
+                    /><br />
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    value={LoginPassword}
+                    autoComplete="password"
+                    onChange={this.onTextboxChangeLoginPassword}
+                    /><br />
+                    {' '}
+                    <div>
+                        <button onClick={this.onSignIn}>Sign In</button>
+                        {' '}
+                        <Link to="/register"> Register </Link>
+                    </div>
+                    {' '}
+                </div>
             </div>
         );
     }
