cbf381a1ca928a0697168dd38a0ccc4442d2c7b5
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-07 14:44:43 +0530

    express-session middleware added

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const user = require("./routes/user");
 const initiatemongoserver = require("./config/db");
+const session = require("express-session");
 
 initiatemongoserver();
 
@@ -21,6 +22,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
   next();
 });
+
+app.use(session({
+  secret: 'Secreyyyy',
+  resave: true,
+  saveUninitialized: true
+}))
 
 // app.get("/", (req, res) => {
 //   res.json({ message: "API Working" });
