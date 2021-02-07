429b8393c9a83c8a0f5296f5451daeac5d3b2247
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-07 14:51:03 +0530

    session request property added in middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,9 +5,13 @@ module.exports = function(req, res, next) {
   if (!token) return res.status(401).json({ message: "Auth Error" });
 
   try {
+    if(req.session.userid){
     const decoded = jwt.verify(token, "randomString");
     req.user = decoded.user;
     next();
+    }else{
+      res.status(500).send({ message: "Invalid Token" });
+    }
   } catch (e) {
     console.error(e);
     res.status(500).send({ message: "Invalid Token" });
