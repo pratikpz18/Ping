514820d9a3b9145b9b515e89ad06048fb721ec00
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-09 14:18:56 +0530

    express session in not used for now

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,13 +5,9 @@ module.exports = function(req, res, next) {
   if (!token) return res.status(401).json({ message: "Auth Error" });
 
   try {
-    if(req.session.userid){
-    const decoded = jwt.verify(token, "randomString");
-    req.user = decoded.user;
-    next();
-    }else{
-      res.status(500).send({ message: "Invalid Token" });
-    }
+      const decoded = jwt.verify(token, "randomString");
+      req.user = decoded.user;
+      next();
   } catch (e) {
     console.error(e);
     res.status(500).send({ message: "Invalid Token" });
