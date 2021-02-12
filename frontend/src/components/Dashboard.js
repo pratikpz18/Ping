dfa0f4c01e7ea899167ad99545a4e2782d1e818d
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-12 23:26:00 +0530

    dashboard access restricted

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -16,46 +16,60 @@ export default class Registration extends Component{
 
     }
 
-    componentDidMount() {
-        this.state = {
-            isLoading:true
-        };
-    };
-
     logOut() {
         UserService.logout()
     }
 
     render(){
-        const { currentUser } = this.state;
+        const { currentUser ,isLoading } = this.state;
         console.log(currentUser)
 
-        return(
-            <div>
-                <h1>Dashboard</h1>
-                {' '}
+        if (isLoading) {
+            return (<div><p>Loading...</p></div>);
+        }
+
+        if(!currentUser){
+            return(
                 <div>
                     <div>
-                        <header className="jumbotron">
-                            <h3>
-                                Welcome<strong>{'  '}{currentUser.user.username}</strong>
-                            </h3>
-                        </header>
+                        <h1>You Need to Login....</h1>
                     </div>
-                    {' '}
-                    <p>
-                        <strong>Id:</strong>{" "}
-                        {currentUser.user._id}
-                    </p>
-                    <p>
-                        <strong>Email:</strong>{" "}
-                        {currentUser.user.email}
-                    </p>
+                    <div>
+                        <Link to="/login" > Log In </Link>
+                    </div>
                 </div>
+            )
+        }
+        else{
+            return(
                 <div>
-                    <a href="/login" onClick={this.logOut}>LogOut</a>
+                    <h1>Dashboard</h1>
+                    {' '}
+                    <div>
+                        <div>
+                            <div>
+                                <header className="jumbotron">
+                                    <p>
+                                        Welcome<strong>{'  '}{currentUser.user.username}</strong>
+                                    </p>
+                                </header>
+                            </div>
+                            {' '}
+                            <p>
+                                <strong>Id:</strong>{" "}
+                                {currentUser.user._id}
+                            </p>
+                            <p>
+                                <strong>Email:</strong>{" "}
+                                {currentUser.user.email}
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <a href="/login" onClick={this.logOut}>LogOut</a>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
