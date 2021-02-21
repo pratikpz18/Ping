690dcaa8344ebcd4b0269a2ff718b6e44a67ec95
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-02-21 12:38:02 +0530

    socket.io initialized

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const user = require("./routes/user");
 const initiatemongoserver = require("./config/db");
-const session = require("express-session");
 
 initiatemongoserver();
 
@@ -12,9 +11,8 @@ const app = express();
 // PORT
 const PORT = process.env.PORT || 4000;
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/'));
 
 app.use(function(req, res, next) {
@@ -22,20 +20,18 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
   next();
 });
+app.use(cors());
 
-// app.use(session({
-//   secret: 'Secreyyyy',
-//   resave: false,
-//   saveUninitialized: true
-// }))
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "API Working" });
-// });
-
-app.listen(PORT, (req, res) => {
+const server = app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
 
-app.use(cors());
-app.use("/users/", user);
+const io = require("socket.io").listen(server);
+
+// Assign socket object to every request
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
+});
+
+app.use("/users", user);
