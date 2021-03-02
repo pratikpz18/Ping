a5806f5bcbc2ae43d58de54d03171de2bc845dbe
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-03-02 13:21:11 +0530

    Add Friend function added and connected to backend

--- frontend/src/services/SearchService.js
+++ frontend/src/services/SearchService.js
@@ -5,3 +5,8 @@ export const SearchUser = (data) => {
     return axios.post('http://localhost:4000/users/dashboard/search',data)
         .then(res => res.data);
 };
+
+export const AddFreind = (data) => {
+    return axios.post('http://localhost:4000/users/dashboard/search/addfriend',data)
+                .then(res=> res.data)
+  }
