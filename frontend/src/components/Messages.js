d92160cc2b9410ca4ad49dab0947186fa3d3c593
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-03-12 00:44:49 +0530

    halfway done msg is sending and receiving working on this currentlly

--- frontend/src/components/Messages.js
+++ frontend/src/components/Messages.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react';
 import { Link,Redirect } from 'react-router-dom';
 import UserService from "../services/userservice";
 import {getUsersFriend} from "../services/messageservice";
+import io from "socket.io-client";
+const SOCKET_IO_URL = "http://localhost:4000/";
 
 export default class Messages extends Component {
     constructor(props){
         super(props)
-
+        this.socket = io(SOCKET_IO_URL)
         this.state = {
             currentUser: UserService.getCurrentUser(),
             isLoading:false,
             userdetails:[],
+            show:false,
+            username:'',
+            message:'',
+            socketConnected:false,
+            messages:[]
         };
+        this.onTextboxChangeMessage = this.onTextboxChangeMessage.bind(this)
     }
 
     componentDidMount(){
         this.fetchUser()
+        this.socket.on('connect',()=> {
+            this.setState({ socketConnected : true})
+            // console.log("connection")
+        })
+        // this.socket.emit('send',{username:this.username,message:this.message,senderusername:currentUser.user.username});
+        this.socket.on('send',(data)=>{
+            console.log('component did mount',data)
+            // if(data.message.length>0){
+                this.setState({messages:[...this.state.messages,data]})
+            // }
+            
+        })
     }
 
     async fetchUser(){
@@ -26,16 +46,66 @@ export default class Messages extends Component {
             console.log(data)
             let user = await getUsersFriend(data)
             this.setState({ userdetails: user });
-            console.log(user)
+            // console.log(user)
         }catch(err){
             console.log(err)
         }
     }
 
+    showMessageSpace(elementusername){
+        const {currentUser} =this.state
+        this.setState({
+            show: true,
+            username:elementusername
+          });
+        // console.log(elementusername)
+        this.SendMessage(this.username,this.message,currentUser.user.username)
+    }
+
+    onTextboxChangeMessage(e){
+        this.setState({ message:e.target.value})
+    }
+
+    SendMessage(username,message,senderusername){
+        const {messages} =this.state
+        if(this.state.socketConnected){
+            // this.socket.on("New connection", () => {
+            //     console.log('New connection from client side')
+            // });
+            console.log('if condition test',username,message,senderusername )
+            this.socket.emit('send',{username,message,senderusername});
+            // this.socket.on('send',(data)=>{
+            //     console.log(data)
+            //     this.setState({messages:[...this.state.messages,data]})
+            // })
+            console.log('condition username',`${username}`,  )
+            this.socket.on(`${username}`, (d)=>{
+                // console.log('test from receive username', d)
+                // if(message?.lenght>0) {
+                    this.setState({messages:[...messages,d]})
+                    // this.setState({[`${username}`]:[...this.state[`${username}`], d]})
+                // }
+                // else{
+                //     // this.setState({[`${username}`]:[d]})
+                //     this.setState({messages:[d]})
+                // }
+               
+            })
+            // this.socket.on("message", (d) => {
+            //     this.socket.on(`${this.username}`, (d)=>{
+            //         console.log('test from receive username', d)
+            //         this.setState({messages:[...messages,d]})
+            //     })
+            // });
+        }
+        this.setState( { message:'' })
+    }
+
+    
 
     render(){
-        const { currentUser ,isLoading,userdetails } = this.state;
-
+        const { currentUser ,isLoading,userdetails,message,messages} = this.state;
+        console.log(messages)
         if (isLoading) {
             return (<div><p>Loading...</p></div>);
         }
@@ -58,20 +128,54 @@ export default class Messages extends Component {
                         {userdetails.map((element) => {
                             return(
                                 <div key={element._id}>
-                                    <li>{element.username}{' '}<input 
+                                    <li><Link to={`/dashboard/profile/:${element._id}`}>{element.username}</Link>{' '}<input 
                                     type="button" 
                                     id={element._id}
-                                    value="MEssage"></input></li>
+                                    value="Message"
+                                    onClick={this.showMessageSpace.bind(this,element.username)} ></input></li>
                                 </div>
-                        );
+                            );
                         })
-                    }
+                        }
                     </ul>
                     {' '}
                 </div>
                 {' '}
                     <Link to="/dashboard">Dashboard</Link>
                 {' '}
+                <div>
+                {
+                    this.state.show &&
+                    (<div>
+                        <h2>Username : {' '}{this.state.username}</h2>
+                        {' '}
+                        <div>
+                            <h3>Body</h3>
+                            <div>
+                                <ul>
+                                {/* { this.state[`${this.state.username}`]?.map((msg,key) =>{ */}
+                                {messages.map((msg,key) =>{
+                                    return(<li key={key}><span>{msg?.message}</span></li>);
+                                })
+                                }
+                                </ul>
+                            </div>
+                        </div>
+                        {' '}
+                        <div>
+                            {' '}
+                            <input 
+                            type="text"
+                            name="message"
+                            value={message}
+                            onChange={this.onTextboxChangeMessage}
+                            ></input>
+                            <button className='btn btn-info' onClick={this.SendMessage.bind(this,this.state.username,this.state.message,currentUser.user.username )}>Send</button>
+                        </div>
+                        {' '}
+                    </div>)
+                    }
+                </div>
             </div>
         )
         }
