2fa9d0e5909ca420f45e2df2e1da53e4b2681670
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-03-12 00:30:45 +0530

    Profile component changed all users profile can be seen

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -6,23 +6,40 @@ export default class Profile extends Component{
 
     constructor(props) {
         super(props);
-    
         this.state = {
-            currentUser: UserService.getCurrentUser(),
-            isLoading:false
+            currentUser:[],
+            isLoading:false,
         };
-
         this.logOut = this.logOut.bind(this);
-
     }
 
     logOut() {
         UserService.logout()
+    } 
+
+    componentWillMount(){
+        let userid = this.props.match.params.userid;
+        userid=userid.slice(1)
+        console.log('First this called');
+        this.fetchUser(userid)
+      }
+
+    async fetchUser(userid){
+        try{
+            console.log(userid)
+            let user = await UserService.getUserById(userid)
+            this.setState({ currentUser:user})
+        }catch(err){
+            console.log(err)
+        }
+        
     }
 
     render(){
         const { currentUser ,isLoading } = this.state;
+        const k=Object(currentUser.friendsList)
         console.log(currentUser)
+        
 
         if (isLoading) {
             return (<div><p>Loading...</p></div>);
@@ -45,18 +62,21 @@ export default class Profile extends Component{
                             <div>
                                 <header className="jumbotron">
                                     <p>
-                                        Welcome<strong>{'  '}{currentUser.user.username}</strong>
+                                        Welcome<strong>{'  '}{currentUser.username}</strong>
                                     </p>
                                 </header>
                             </div>
                             {' '}
                             <p>
                                 <strong>Id:</strong>{" "}
-                                {currentUser.user._id}
+                                {currentUser._id}
                             </p>
                             <p>
                                 <strong>Email:</strong>{" "}
-                                {currentUser.user.email}
+                                {currentUser.email}
+                            </p>
+                            <p>
+                                <strong>Friends:</strong>{' '}{ k.length }
                             </p>
                         </div>
                     </div>
