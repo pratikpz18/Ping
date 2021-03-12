2891fa949487dd10b3af4e359e062c27f3fb08a0
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-03-12 11:50:11 +0530

    halfway done msg is sending and receiving working on this currently

--- backend/index.js
+++ backend/index.js
@@ -20,18 +20,40 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
   next();
 });
+
 app.use(cors());
 
 const server = app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
 
-const io = require("socket.io");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*',
+  }
+});
 
 // Assign socket object to every request
 app.use(function (req, res, next) {
   req.io = io;
   next();
+});
+
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+  console.log('connection established',socket.id);
+  // io.emit('New Connection')
+  socket.on('send', (data)=>{
+    console.log("Receive data from single username",data)
+    socket.join(`${data.username}`) //IF I COMMENT THIS USERNAME IS TOGGLING BUT MSG OF PREVIOUS USER IS STILL THERE
+    // io.emit(`${data.username}`, data)
+    io.emit('send',data)
+    io.sockets.in(`${data.username}`).emit(`${data.username}`,data)
+    // socket.to(`${data.username}`).emit('message',data.message);
+  });
+  
+  socket.on('disconnect',()=>{
+    io.emit('message','user left')
+  })
 });
 
 app.use("/users", user);
