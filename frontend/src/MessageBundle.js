7ae77444daef0f5eb1e5636b1f894b60d8c3d8a6
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-03-25 16:47:21 +0530

    removed

--- frontend/src/MessageBundle.js
+++
@@ -1,5 +0,0 @@
-export const REGISTRATION_MESSAGE = 'Registered Successfully';
-export const ERROR_IN_REGISTRATION = 'Error in Registration';
-
-export const LOGIN_MESSAGE = 'Login Successful';
-export const ERROR_IN_LOGIN = 'Error in Login'
